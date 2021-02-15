de4f0552b0aafeb32795e3232b040f2217ddabed
Forrest Herman <58410716+forrest-herman@users.noreply.github.com>
2021-02-15 15:42:58 -0500

    weather app and api fetch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+
+const api = { key: "7d3cae9df013e6b21f5ddf71fdf1b9cc", base: "https://api.openweathermap.org/data/2.5/" }
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [query, setQuery] = useState("")
+    const [weather, setWeather] = useState({})
+
+    const search = (evt) => {
+        if (evt.key == "Enter") {
+            fetch(`${api.base}weather?q=${query}&units=metric&appid=${api.key}`)
+                .then((res) => res.json())
+                .then((result) => {
+                    setWeather(result)
+                    setQuery("")
+                    console.log(result)
+                })
+        }
+    }
+
+    const dateBuilder = (date) => {
+        let currentDate = String(date)
+        let day = currentDate.slice(3, 15)
+        let hour = (hr) => {
+            return hr > 12 ? hr - 12 : hr
+        }
+        let time = hour(parseFloat(currentDate.slice(16, 19))) + ":" + currentDate.slice(19, 21)
+        return `${day} at ${time}` //day + " at " + time
+    }
+
+    return (
+        <div className='app'>
+            <main>
+                <div className='search-box'>
+                    <input
+                        type='text'
+                        className='search-bar'
+                        placeholder='Search...'
+                        onChange={(e) => setQuery(e.target.value)}
+                        value={query}
+                        onKeyPress={search}
+                    ></input>
+                </div>
+                {typeof weather.main != "undefined" ? (
+                    <>
+                        <div className='location-box'>
+                            <div className='location'>
+                                {weather.name}, {weather.sys.country}
+                            </div>
+                            <div className='date'>{dateBuilder(new Date())}</div>
+                        </div>
+                        <div className='weather-box'>
+                            <div className='temperature'>{Math.round(weather.main.temp)}°C</div>
+                            <div className='weather'>{weather.weather[0].main}</div>
+                        </div>
+                    </>
+                ) : (
+                    ""
+                )}
+            </main>
+        </div>
+    )
 }
 
-export default App;
+export default App
